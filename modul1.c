4d29fa2a4bb13859e46b68085563268f3b38eb54
Celvin Saputra <145645853+Celcouzz@users.noreply.github.com>
2023-09-29 17:54:52 +0700

    Create modul1.c

---
+++ modul1.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+int main() {
+	int a = 12 , B = 28 ;
+	float c = 2.5 + d = 8.9
+	double e = 'R';
+	
+	(tambahkan komentar disini)
+	int sum = a + (b * c) - d;
+	printf("Bilangan a: %d\n", a);
+	printf("Bilangan b: %d\n", b);
+	printf("Bilangan c: %f\n", d);
+	printf("Bilangan d: %f\n", d);
+	printf("Karakter e: %c\n", e);
+	printf("hasil: %f/n", double");
+	return 0;
+}
